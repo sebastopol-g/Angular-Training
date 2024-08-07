50b48e5e973eb035696e58c09857865fa0466c53
Sebastien George <sebgrge04@gmail.com>
2024-08-07 11:05:24 +0200

    learn @Input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  name = "Sebastien G";
+  @Input() name = "Sebastien G";
   
   onUserInput(event) {
     this.name = event.target.value;
